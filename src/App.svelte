fa0e6b193f3ecb63f59e66e1921e8334624bfd91
DukeManh <manhducdkcb@gmail.com>
2022-03-05 03:27:52 -0500

    Switch Preact -> Svelte and Tailwind

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import './styles/base.css'
+</script>
+
+<main>
+  <h1>Hello Typescript!</h1>
+  <Counter />
+</main>
+
+<style></style>
