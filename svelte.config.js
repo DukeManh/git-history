5f5886b2b46b139948676b86e805331aa3da8867
DukeManh <manhducdkcb@gmail.com>
2022-03-19 16:35:29 -0400

    User static svelte adapter, disable ssr

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,12 +11,9 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'app.html'
-		}),
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+			fallback: 'app.html',
+			precompress: true
+		})
 	}
 };
 
