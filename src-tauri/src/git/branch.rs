17c03b9350f9c116618a550bdfba7e7c6db94932
DukeManh <manhducdkcb@gmail.com>
2022-04-17 18:24:11 -0400

    Tauri: comment out dead code in branch.rs

--- src-tauri/src/git/branch.rs
+++ src-tauri/src/git/branch.rs
@@ -1,5 +1,5 @@
-use crate::git;
+// use crate::git;
 
-pub fn get_branches(local_repo: &String) -> String {
-  return git::cli::spawn(&String::from("branch"), local_repo);
-}
+// // pub fn get_branches(local_repo: &String) -> String {
+// //   return git::cli::spawn(&String::from("branch"), local_repo);
+// // }
